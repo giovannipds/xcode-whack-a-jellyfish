43f2f59bdeec5317b5006ed6c2262d2c53afcad1
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-06-21 15:35:51 -0300

    Make a sceneView that add a SCNBox

--- Whack a jellyfish/ViewController.swift
+++ Whack a jellyfish/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var SceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.SceneView.session.run(configuration)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode() {
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0, 0, -1)
+        self.SceneView.scene.rootNode.addChildNode(node)
+    }
+    
 
 }
 
